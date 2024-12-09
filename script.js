38d8b0f38064a03e34b2b82d27178a8a6e30c0ca
alelomuz <133164026+alelomuz@users.noreply.github.com>
2024-12-09 11:50:55 +0100

    v8

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
 // Translations object
 const translations = {
     en: {
-        title: "Wedding of Silvia and Joachim",
+        title0: "Wedding of Silvia and Joachim",
         site1: "Location",
         site2: "Accommodations",
         site3: "Plan",
         site4: "Travel Informations",
         site5: "Contacts",
+        title1: "Ceremony",
     },
     de: {
-        title: "Hochzeit von Silvia und Joachim",
+        title0: "Hochzeit von Silvia und Joachim",
         site1: "Veranstaltungsort",
         site2: "Unterkünfte",
         site3: "Plan",
         site4: "Reiseinformationen",
         site5: "Kontakte",
+        title1: "Traungsfeier",
     },
     it: {
-        title: "Matrimonio di Silvia e Joachim",
+        title0: "Matrimonio di Silvia e Joachim",
         site1: "Location",
         site2: "Alloggi",
         site3: "Piano",
         site4: "Informazioni di viaggio",
         site5: "Contatti",
+        title1: "Ceremonia e ricevimento",
     }
 };
 
 // Function to update content based on language
 function updateLanguage(lang) {
-    document.getElementById("title").textContent = translations[lang].title;
+    document.getElementById("title0").textContent = translations[lang].title0;
     document.getElementById("site1").textContent = translations[lang].site1;
     document.getElementById("site2").textContent = translations[lang].site2;
     document.getElementById("site3").textContent = translations[lang].site3;
     document.getElementById("site4").textContent = translations[lang].site4;
     document.getElementById("site5").textContent = translations[lang].site5;
+    document.getElementById("title1").textContent = translations[lang].title1;
 }
 
 // Language switcher event listener
@@ -43,5 +47,5 @@ languageSelect.addEventListener("change", (event) => {
     updateLanguage(selectedLanguage);
 });
 
-// Initialize the page with default language (English)
-updateLanguage("en");
+// Initialize the page with default language
+updateLanguage("it");
