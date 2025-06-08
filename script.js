131e64b45cdde329d2e98bbd273d7c9cd70a9161
alelomuz <133164026+alelomuz@users.noreply.github.com>
2025-06-08 18:53:00 +0200

    Update translation for wedding timeline sections to enhance clarity

--- script.js
+++ script.js
@@ -66,8 +66,8 @@ const translations = {
         description2: "Wir planen ein Bus für den Transport von Porto degli Argonauti nach Notedimare und zurück zu organisieren. Weitere Infos werden folgen.",
         timeline2n1: "TRAUUNG",
         timeline2n2: "FOTOS",
-        timeline2n3: "APERITIF",
-        timeline2n4: "MITTAGSESSEN",
+        timeline2n3: "GROßEN APERITIF BUFFET MIT LIVE-COOKING",
+        timeline2n4: "HOCHZEITSESSEN",
         timeline2n5: "HOCHZEITSTORTE UND\nDESSERTBUFFET",
         timeline2n6: "AFTERPARTY MIT DJ\nUND COCKTAILBAR",
         /* Section 3 */
